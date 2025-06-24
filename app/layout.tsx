94b2cd1f9fd1f64bcb38b35d96e270dede7dd160
João Victor Santana <141521903+jv-santana@users.noreply.github.com>
2025-06-24 20:58:25 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Users Posts',
   description: 'A simple app to view users posts',
-  generator: 'Alana Rocha',
+  generator: 'Joao Victor',
 }
 
 export default function RootLayout({
